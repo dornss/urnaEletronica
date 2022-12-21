0d084ee7719e9e7a1c53f6857919eb3ae169584b
Gabriel Dornelas <gabrielldornelas@hotmail.com>
2022-12-21 15:29:38 -0300

    Implementação do código do projeto

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'DEP. ESTADUAL',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '18100',
+                nome: 'Daniel do Salve',
+                partido: 'REDE',
+                fotos:[
+                    {url:'18100.jpg', legenda: 'Dep. Estadual'}
+                ]
+            },
+            {
+                numero: '13113',
+                nome: 'João Paulo',
+                partido: 'PT',
+                fotos:[
+                    {url:'13113.jpeg', legenda: 'Dep. Estadual'}
+                ]
+            },
+        ]   
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'Lula',        
+                partido: 'PT',
+                vice: 'Alckmin',
+                fotos:[
+                    {url:'13.jpg', legenda: 'Presidente'},
+                    {url:'13_2.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '27',
+                nome: 'Eymael',
+                partido: 'DC',
+                vice: 'Professor Bravo',
+                fotos:[
+                    {url:'27.jpg', legenda:'Presidente'},
+                    {url:'27_2.jpeg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+        ]
+    }
+];
